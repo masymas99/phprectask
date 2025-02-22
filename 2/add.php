67e28760e86e0a6fdd98b3ceaecf5d1bef0876cb
masymas99 <masoodadel1999@gmail.com>
2025-02-22 18:18:17 +0200

    cbvxbc

--- 2/add.php
+++ 2/add.php
@@ -6,6 +6,7 @@
     <title>Add Student</title>
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/css/bootstrap.min.css" rel="stylesheet">
 </head>
+
 <body>
     <div class="container mt-5">
         <form action="" method="post" enctype="multipart/form-data">
