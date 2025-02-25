3836023bff3a88daf3ed29de305dbec2b08ec8c5
masymas99 <masoodadel1999@gmail.com>
2025-02-25 21:34:36 +0200

    "Removed validation check for numeric ID in delete.php"

--- 3/delete.php
+++ 3/delete.php
@@ -2,9 +2,8 @@
 include("db.php");
 $db = new Database();
 
-// Validate and sanitize the ID parameter
 if(isset($_GET["id"]) && is_numeric($_GET["id"])) {
-    $id = intval($_GET["id"]); // Convert to integer for safety
+    $id = intval($_GET["id"]); 
     $sql = $db->deleteEmployee($id);
     header("Location: view.php");
 } else {
